03953a85e109dd19507c7fcf7d01c84231d3f514
Dodojonov Munirjon <munirhuja1357@gmail.com>
2025-09-30 12:38:51 +0500

    fix: fix docker and start work

---
+++ python-ml/main.py
@@ -0,0 +1,13 @@
+# main.py
+
+import time
+
+def main():
+    print("Python ML service started")
+    # Пример бесконечного цикла, имитирующего работу сервиса
+    while True:
+        print("Service is running...")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
